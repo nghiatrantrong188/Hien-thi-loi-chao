37abb92f9fc0b053b124838e8e2af582150d5431
Trần Trọng Nghĩa <nghiatrantrong188@gmail.com>
2023-03-31 14:55:39 +0700

    Hien Thi Loi Chao

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name: ");
+
+            string yourName = Console.ReadLine();
+
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
